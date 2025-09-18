a1bace3cb69083f3346c07c09ea485a33807d874
Almas Khan <khanalmas753@gmail.com>
2025-09-18 22:22:07 +0200

    day-1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,39 @@
-class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+    // YOUR CODE HERE:
+    class Question {
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    
+// 1. constructor (text, choices, answer, difficulty)
+
+//shuffle the choices randomly
+
+
+    // shuffleChoices() {
+    //     for (let i = this.choices.length -1; i > 0; i--){
+    //         const j =Math.floor(Math.random()* (i))
+    //         [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+    //     }
+        
+    // }
+   
+    // Shuffle choices using while loop
+    shuffleChoices() {
+        let currentIndex = this.choices.length;
+
+        while (currentIndex != 0) {
+            
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            
+            currentIndex--;
+
+            [this.choices[currentIndex], this.choices[randomIndex]] = [
+                this.choices[randomIndex], this.choices[currentIndex]
+            ];
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,57 @@
-class Quiz {
+
     // YOUR CODE HERE:
-    //
+    class Quiz {
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
+    // 3. moveToNextQuestion()
+moveToNextQuestion() {
+    this.currentQuestionIndex++;
+}
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i));
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+    }
+        }
 
     // 5. checkAnswer(answer)
+checkAnswer(answer){
+    if (answer === this.getQuestion().answer) {
+        this.correctAnswers++;
+         return true;
+         
+    } else {
+        return false;
+    }  
+}
+//console.log(this.getQuestion());
+         //  // logs the whole question object
+
+        // console.log(this.getQuestion().answer); 
+       // // logs just the answer value
+        // console.log(this.getQuestion().question);
+ //  // logs just the question text (if property exists)
+
 
     // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex ===this.questions.length) {
+            return true;
+        }
+        }
 }
